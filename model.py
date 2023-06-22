8c1b4fe710d342c32e2202b8c9afd0c3e17c8a8c
Vladyslav Duran <justez4rtz@gmail.com>
2023-06-22 20:14:30 +0300

    feat: add custom layer unit feature and add features for user interaction

--- model.py
+++ model.py
@@ -22,6 +22,10 @@ class DenseLayer(tf.keras.layers.Layer):
         """Method that executes the layer."""
         return tf.matmul(inputs, self.w) + self.b
 
+    def get_config(self):
+        """Method that returns the configuration of the layer."""
+        return {"units": self.units}
+
 
 class CustomModel(tf.keras.Model):
     """Class that represents a custom model."""
@@ -33,7 +37,7 @@ class CustomModel(tf.keras.Model):
         self.custom_layers = []
         for _ in range(num_layers):
             self.custom_layers.append(DenseLayer(50))
-
+        self.num_layers = num_layers
         self.dropout_rate = dropout_rate
         self.output_units = output_units
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
@@ -66,7 +70,28 @@ class CustomModel(tf.keras.Model):
 
     def add_layer(self, units):
         """Method that adds a layer to the model."""
+        input_units = self.custom_layers[-1].units if self.custom_layers else 50
+        output_units = units
+
+        if self.custom_layers:
+            if self.custom_layers[-1].units != input_units:
+                raise ValueError("Incompatible number of units in the previous layer.")
+
         self.custom_layers.append(DenseLayer(units))
+        self.output_layer = DenseLayer(self.output_units)
+
+    def remove_layer(self, layer_index):
+        """Method that removes a layer from the model."""
+        if layer_index == self.num_layers - 1:
+            self.output_layer = DenseLayer(self.custom_layers[-2].units)
+        self.custom_layers.pop(layer_index)
+        self.num_layers -= 1
+
+    def get_layer(self, index=None):
+        """Method that returns a participate layer."""
+        if index is None or index == self.num_layers:
+            return self.output_layer.get_config()
+        return self.custom_layers[index].get_config()
 
     def get_config(self):
         """Method that returns the configuration of the model."""
@@ -77,4 +102,4 @@ class CustomModel(tf.keras.Model):
         }
 
 
-model = CustomModel(1, 0.2, 3)
+model = CustomModel(0, 0.2, 3)
