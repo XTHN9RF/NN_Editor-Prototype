77910cb5ae8692459a77d9ce86261b0755f8f05d
Vladyslav Duran <justez4rtz@gmail.com>
2023-06-22 20:14:49 +0300

    feat: add button to get information about layer

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from tkinter import simpledialog
 
 def main():
     window = tk.Tk()
-    window.title("Neural network editor")
+    window.title("Прототип редактора нейронної мережі")
 
-    test_button = tk.Button(window, text="Button 1", command=lambda: test())
-    accuracy_button = tk.Button(window, text="Button 2", command=lambda: accuracy_scale())
-    add_layer_button = tk.Button(window, text="Button 3",
+    test_button = tk.Button(window, text="Тестувати нейронну мережу", command=lambda: test())
+    accuracy_button = tk.Button(window, text="Оцінка точності передбачення", command=lambda: accuracy_scale())
+    add_layer_button = tk.Button(window, text="Додати шар",
                                  command=lambda: model.add_layer(
-                                     simpledialog.askinteger("Input", "Number of neurons:") or 1))
-    get_config_button = tk.Button(window, text="Button 4", command=lambda: print(model.get_config()))
+                                     simpledialog.askinteger("Додати шар", "Введіть число нейронів(до 50):") or 1))
+    get_config_button = tk.Button(window, text="Переглянути конфігурацію мережі",
+                                  command=lambda: print(model.get_config()))
+    get_layer_button = tk.Button(window, text="Переглянути шар",
+                                 command=lambda: print(model.get_layer(
+                                     simpledialog.askinteger("Переглянути шар", "Введіть номер шару:") or 0)))
 
-    for button in (test_button, accuracy_button, add_layer_button, get_config_button):
+    for button in (test_button, accuracy_button, add_layer_button, get_config_button, get_layer_button):
         button.config(relief=tk.RAISED, bd=3, padx=10, pady=5, bg="blue", fg="white", font=("Arial", 12), width=30)
         button.config(activebackground="green", activeforeground="white", highlightcolor="blue", highlightthickness=3)
 
@@ -24,6 +28,7 @@ def main():
     accuracy_button.pack(pady=10)
     add_layer_button.pack(pady=10)
     get_config_button.pack(pady=10)
+    get_layer_button.pack(pady=10)
 
     window.mainloop()
 
